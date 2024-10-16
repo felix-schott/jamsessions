961a3cfb031e567eb8380cd432ec857f51cb202f
Felix Schott <felix.schott@proton.me>
2024-10-16 17:58:42 +0200

    change test-ci command, less verbose

--- backend/cmd/server/handlers_test.go
+++ backend/cmd/server/handlers_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -52,6 +53,8 @@ func checkResultSetForSessionIds(t *testing.T, sessionIds []int32, body types.Se
 }
 
 func TestHandlers(t *testing.T) {
+
+	slog.SetLogLoggerLevel(slog.LevelError) // change this to see more log informations
 
 	// setup database connection
 	pool, err := dbutils.CreatePool(ctx)
