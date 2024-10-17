b1354ab418d1993ce50751d52b432acdfd880d24
Felix <122696847+felix-schott@users.noreply.github.com>
2024-10-17 16:38:47 +0100

    adding reverse proxy configuration, deployment architecture fixes (#10)
    * fix issue #9
    
    * separate production dockerfiles, fix frontend production setup
    
    * add changelog, preparing for first release (testing the pipelines)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	define: {
-		"process.env.API_ADDRESS": JSON.stringify(process.env.API_ADDRESS),
-		"process.env.TILES_ADDRESS": JSON.stringify(process.env.TILES_ADDRESS),
+		"process.env.API_ADDRESS": process.env.API_ADDRESS ? JSON.stringify(process.env.API_ADDRESS) : JSON.stringify("/api"),
+		// "process.env.TILES_ADDRESS": JSON.stringify(process.env.TILES_ADDRESS),
 	},
 	plugins: [sveltekit()]
 });
