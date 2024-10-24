d0f8787e72f65bb657ca251431bba3e1950342c1
Felix <122696847+felix-schott@users.noreply.github.com>
2024-10-24 16:58:53 +0100

    Deployment bugs (#27)
    fix a few deployment bugs, download missing db init scripts in install.sh, create .env template

--- deploy/install.sh
+++ deploy/install.sh
@@ -16,9 +16,15 @@ echo "Downloading dbcli binary to ./bin/dbcli"
 mkdir -p -m 755 $directory/bin
 wget -q -O $directory/bin/dbcli "https://github.com/felix-schott/jamsessions/releases/download/$tag/dbcli"
 
-# download run-migrations.sh script
+# download other files needed
 echo "Downloading run-migrations.sh script"
 wget -q -O $directory/run-migrations.sh "https://raw.githubusercontent.com/felix-schott/jamsessions/refs/tags/$tag/backend/scripts/run-migrations.sh"
+
+mkdir -p -m 755 $directory/init_db
+echo "Downloading db-init scripts"
+wget -q -O $directory/init_db/001_schema.sql "https://raw.githubusercontent.com/felix-schott/jamsessions/refs/tags/$tag/backend/internal/db/schema.sql"
+
+wget -q -O $directory/init_db/002_roles.sh "https://raw.githubusercontent.com/felix-schott/jamsessions/refs/tags/$tag/backend/internal/db/scripts/add-roles.sh"
 
 echo "Downloading production docker compose file"
 wget -q -O $directory/docker-compose.yml "https://raw.githubusercontent.com/felix-schott/jamsessions/refs/tags/$tag/deploy/prod.docker-compose.yml"
@@ -28,6 +34,17 @@ wget -q -O $directory/Caddyfile "https://raw.githubusercontent.com/felix-schott/
 
 echo "Downloading prometheus.yml"
 wget -q -O $directory/prometheus.yml "https://raw.githubusercontent.com/felix-schott/jamsessions/refs/tags/$tag/deploy/prometheus.yml"
+
+[[ ! -f $directory/.env ]] && {
+    echo ".env doesn't exist yet - creating file and default directories"
+    touch $directory/.env
+    echo "POSTGRES_DATA_DIR=$directory/postgres-data" > $directory/.env
+    echo "PROD_UID=$UID" >> $directory/.env
+    echo "PROD_GID=$UID" >> $directory/.env
+    mkdir -p $directory/postgres-data
+    mkdir -p $directory/migrations/suggestions
+    mkdir -p $directory/migrations/archive
+}
 
 echo "Creating readme file with instructions"
 cat << EOF > $directory/README.md
@@ -40,6 +57,7 @@ First, make sure there is a .env file present in $directory that contains the fo
 - POSTGRES_DB (name of the database)
 - PROD_UID (host uid that you want files in volumes to be owned by)
 - PROD_GID (group id of image user)
+- WEBSITE_HOST (the domain of the website to enable auto https, 0.0.0.0 for local bind)
 
 If you wish, you can modify the docker-compose.yml file according to your needs. Note that running the default docker-compose won't work
 if you're not the project owner, and you will have to build your own production docker images.
@@ -53,4 +71,4 @@ will write little bash scripts to $directory/migrations that make use the dbcli 
 Review the script contents and use the run-migrations.sh to apply all changes.
 EOF
 
-echo "Finished installation process - please consule the generate README file for further instructions."
+echo "Finished installation process - please consult the generated README file for further instructions."
