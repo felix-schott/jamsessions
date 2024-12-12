be4f1977ca9ba3c7356a701959de827186af1234
Felix Schott <felix.schott@proton.me>
2024-12-12 12:18:22 +0100

    close #105, #102

--- deploy/migrations-alert.sh
+++ deploy/migrations-alert.sh
@@ -2,18 +2,31 @@
 
 set -eo pipefail
 
+# load .env file if it exists
+[[ ! -f .env ]] || set -a && source .env && set +a
+
 [[ $1 == "" ]] && echo "Please provide the target directory of the installation as a positional argument." 1>&2 && exit 1;
-directory=$1/migrations
+directory=$1
 
 [[ $TELEGRAM_TOKEN == "" ]] && echo "Please provide the environment variable TELEGRAM_TOKEN." 1>&2 && exit 1;
 [[ $TELEGRAM_CHAT_ID == "" ]] && echo "Please provide the environment variable TELEGRAM_CHAT_ID." 1>&2 && exit 1;
 
 send_alert () {
-  curl -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage -d '{"chat_id":'${TELEGRAM_CHAT_ID}',"text":"PENDING MIGRATIONS"}' -H 'Content-Type: application/json'
+  type=$1
+  echo "Sending $type alert"
+  curl -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage -d '{"chat_id":'${TELEGRAM_CHAT_ID}',"text":"PENDING '$type'"}' -H 'Content-Type: application/json'
 }
 
-if [[ -d $directory ]]; then
-    ls $directory/*.sh &> /dev/null && send_alert &> /dev/null
+echo "Inspecting $directory/migrations directory"
+if [[ -d "$directory/migrations" ]]; then
+    ls $directory/migrations/*.sh &> /dev/null && send_alert "MIGRATIONS" &> /dev/null
 else
-    echo "Directory $directory does not exist" 1>&2 && exit 1
+    echo "Directory $directory/migrations does not exist" 1>&2 && exit 1
+fi
+
+echo "Inspecting $directory/migrations/suggestions directory"
+if [[ -d "$directory/migrations/suggestions" ]]; then
+    ls $directory/migrations/suggestions/*.sh &> /dev/null && send_alert "SUGGESTIONS" &> /dev/null
+else
+    echo "Directory $directory/migrations/suggestions does not exist" 1>&2 && exit 1
 fi
